e4a51ac4c8a83b01946d504af7843db7bbeb9890
Thomas Ung <tomkimsour@gmail.com>
2024-12-03 21:58:59 +0100

    remove useless iterators

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,15 +3,9 @@ advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let res = input
-        .lines()
-        .fold(0,|acc,line| {
-            acc + re.captures_iter(line)
-                .fold(0, |acc, cap|
-                {
-                    acc + cap[1].parse::<u32>().unwrap() * cap[2].parse::<u32>().unwrap()
-                })
-        });
+    let res = re.captures_iter(input).fold(0, |acc, cap| {
+        acc + cap[1].parse::<u32>().unwrap() * cap[2].parse::<u32>().unwrap()
+    });
     Some(res)
 }
 
@@ -19,16 +13,17 @@ pub fn part_two(input: &str) -> Option<u32> {
     let re_digit = Regex::new(r"(\d+),(\d+)").unwrap();
     let re = Regex::new(r"(mul\((\d+),(\d+)\)|don\'t\(\)|do\(\))").unwrap();
     let mut skip = false;
-    let res = re.captures_iter(input).fold(0, |mut acc,capture| {
-        if capture.get(1).unwrap().as_str() == "don't()"{
+    let res = re.captures_iter(input).fold(0, |mut acc, capture| {
+        if capture.get(1).unwrap().as_str() == "don't()" {
             skip = true;
-        } else if capture.get(1).unwrap().as_str() == "do()"{
+        } else if capture.get(1).unwrap().as_str() == "do()" {
             skip = false;
         }
         if !skip {
-            acc += re_digit.captures_iter(capture.get(1).unwrap().as_str())
-            .fold(0 , |acc, cap|{
-                acc + cap[1].parse::<u32>().unwrap() * cap[2].parse::<u32>().unwrap()
+            acc += re_digit
+                .captures_iter(capture.get(1).unwrap().as_str())
+                .fold(0, |acc, cap| {
+                    acc + cap[1].parse::<u32>().unwrap() * cap[2].parse::<u32>().unwrap()
                 });
         }
         acc
