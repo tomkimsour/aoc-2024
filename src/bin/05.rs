e439df53f34d84cd2288f06f495f76c644ae3f8d
Thomas Ung <tomkimsour@gmail.com>
2025-01-03 00:41:42 +0100

    trying to parse ....

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,6 +1,29 @@
 advent_of_code::solution!(5);
+use std::collections::HashMap;
+use itertools::Itertools;
+
+
+pub fn parse_input(
+    input: &str,
+) -> (
+    impl Iterator<Item = (u32, u32)> + '_,
+    impl Iterator<Item = Iterator<Item = u32>> + '_,
+) {
+    let (ordering_rules, updates) = input.split_once("\n\n").unwrap();
+    let or = ordering_rules.lines().map(|line| {
+        let (left, right) = line.split_once('|').unwrap();
+        (left.parse::<u32>().unwrap(), right.parse::<u32>().unwrap())
+    });
+
+    let up = updates
+        .lines()
+        .map(|line| line.split(",").map(|val| val.parse::<u32>().unwrap()));
+    (or, up)
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let (ordering_rule, update) = parse_input(input);
+    let forward_map: HashMap<u32, Vec<u32>> = HashMap::new();
     None
 }
 
@@ -15,7 +38,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(143));
     }
 
     #[test]
